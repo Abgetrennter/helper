6b0e81031922d0b78486f5b1154e6bac9c165076
Abget-phi <Abget-phi@outlook.com>
2024-07-09 01:56:27 +0800

    GUI 1.0

--- helper/main.py
+++ helper/main.py
@@ -1,6 +1,8 @@
 import flet as ft
 import pypinyin as py
 from enum import Enum
+import json
+from collections import namedtuple
 
 
 class ButtonState(Enum):
@@ -48,6 +50,10 @@ class text_button(ft.CupertinoFilledButton):
         self._text.update()
         self.stat = ButtonState.未定义
 
+    @property
+    def value(self):
+        return self._text.value
+
 
 class TextBox(ft.CupertinoFilledButton):
 
@@ -63,13 +69,41 @@ class TextBox(ft.CupertinoFilledButton):
         self._text.color = '#ff5D6673'
 
 
+num2tone = {1: 'ˉ', 2: 'ˊ', 3: 'ˇ', 4: 'ˋ', 5: ' '}
+字音 = namedtuple('字音', '声母 韵母 声调 汉字')
+
+
+class 成语字音:
+    def __init__(self, 成语: str = None, _list=None):
+        self.成语 = None
+        if 成语:
+            self.成语 = 成语
+            self.声母集合 = [i[0] if i[0] else ' ' for i in
+                             py.pinyin(成语, style=py.Style.INITIALS, heteronym=True, strict=False)]
+            ym_t = [[i[0][:-1], i[0][-1]] if i[0][:-1] else [i[0][-1], 5] for i in
+                    py.pinyin(成语, style=py.Style.FINALS_TONE3, heteronym=True, strict=False)]
+            self.韵母集合 = [i[0] if i[0] else ' ' for i in ym_t]
+            self.声调集合 = [num2tone[int(i[1])] for i in ym_t]
+            self.拼音集合 = [字音(*i) for i in zip(self.声母集合, self.韵母集合, self.声调集合, self.成语)]
+        else:
+            self.成语 = "".join(i[-1] for i in _list)
+            self.声母集合 = [i[0] if i[0] else ' ' for i in _list]
+            self.韵母集合 = [i[1] if i[1] else ' ' for i in _list]
+            self.声调集合 = [num2tone[int(i[2])] for i in _list]
+            self.拼音集合 = [字音(*i) for i in zip(self.声母集合, self.韵母集合, self.声调集合, self.成语)]
+
+    def __iter__(self):
+        return iter(self.拼音集合)
+
+
 class P1(ft.Container):
-    def __init__(self, 汉字='草', 声母='c', 韵母='ang', 声调='ˇ'):
-        self.汉字 = text_button(text=汉字, width=80, height=80, size=50)
+    def __init__(self, 字: 字音):
+        self.字 = 字
+        self.汉字 = text_button(text=字.汉字, width=80, height=80, size=50)
         self.汉字.disabled = True
-        self.声母 = text_button(text=声母, width=20, height=25, size=20)
-        self.韵母 = text_button(text=韵母, width=55, height=25, size=20)
-        self.声调 = text_button(text=声调, width=80, height=20, size=30)
+        self.声母 = text_button(text=字.声母, width=20, height=25, size=20)
+        self.韵母 = text_button(text=字.韵母, width=55, height=25, size=20)
+        self.声调 = text_button(text=字.声调, width=80, height=20, size=30)
         super().__init__()
         self.width = 100
         self.height = 150
@@ -80,11 +114,12 @@ class P1(ft.Container):
                 ft.Row([self.汉字], alignment=ft.MainAxisAlignment.CENTER, spacing=0), ],
                 spacing=5, alignment=ft.MainAxisAlignment.CENTER)
 
-    def updates(self, 汉字, 声母, 韵母, 声调):
-        self.汉字.updates(汉字)
-        self.声母.updates(声母)
-        self.韵母.updates(韵母)
-        self.声调.updates(声调)
+    def updates(self, 字: 字音):
+        self.字 = 字
+        self.汉字.updates(字.汉字)
+        self.声母.updates(字.声母)
+        self.韵母.updates(字.韵母)
+        self.声调.updates(字.声调)
 
     def disableds(self):
         self.汉字.disabled = True
@@ -98,26 +133,16 @@ class P1(ft.Container):
         self.韵母.disabled = False
         self.声调.disabled = False
 
-
-num2tone = {1: 'ˉ', 2: 'ˊ', 3: 'ˇ', 4: 'ˋ', 5: ' '}
+    def get_state(self):
+        return self.声母.stat, self.韵母.stat, self.声调.stat
 
 
 class P2(ft.Container):
 
-    def get_pinyin(self, 成语):
-        sm = [i[0] if i[0] else ' ' for i in py.pinyin(成语, style=py.Style.INITIALS, heteronym=True, strict=False)]
-        ym_t = [[i[0][:-1], i[0][-1]] if i[0][:-1] else [i[0][-1], 5] for i in
-                py.pinyin(成语, style=py.Style.FINALS_TONE3, heteronym=True, strict=False)]
-        c = []
-        for i, ii, iii in zip(sm, ym_t, 成语):
-            c.append([iii, i, ii[0], num2tone[int(ii[1])]])
-        return c
-
     def __init__(self, 成语="一败涂地"):
-        self.c = []
+        self.成语字音 = 成语字音(成语)
+        self.c = [P1(h) for h in self.成语字音]
 
-        for i, ii, iii, iiii in self.get_pinyin(成语):
-            self.c.append(P1(汉字=i, 声母=ii, 韵母=iii, 声调=iiii))
         super().__init__()
         self.content = ft.Column(
                 [ft.Row(self.c[:2], alignment=ft.MainAxisAlignment.CENTER, spacing=10),
@@ -126,9 +151,9 @@ class P2(ft.Container):
         )
 
     def updates(self, 成语):
-        for index, value in enumerate(self.get_pinyin(成语)):
-            i, ii, iii, iiii = value
-            self.c[index].updates(i, ii, iii, iiii)
+        self.成语字音 = 成语字音(成语)
+        for index, value in enumerate(self.成语字音):
+            self.c[index].updates(value)
         self.update()
 
     def disableds(self):
@@ -139,13 +164,37 @@ class P2(ft.Container):
         for i in self.c:
             i.enables()
 
+    def get_state(self):
+        """
+        {right: [s, y, t], maybe:[q,w],wrong:[z,x]}×
+        right{s:[(s,0)]}
+        """
+        right = {'声母': [], '韵母': [], '声调': [], '成语': self.成语字音.成语}
+        for i, v in enumerate(self.c):
+            if v.声母.stat == ButtonState.确定位置:
+                right['声母'].append((v.声母.value, i))
+            if v.韵母.stat == ButtonState.确定位置:
+                right['韵母'].append((v.韵母.value, i))
+            if v.声调.stat == ButtonState.确定位置:
+                right['声调'].append((v.声调.value, i))
+        return right
+
 
 def main(page: ft.Page):
+    idioms = {}
+    with open("assets/datas.json", "r", encoding='utf-8') as dataset:
+        raw_idioms = json.load(dataset)
+    #
+    for raw_idiom in raw_idioms:
+        idioms[raw_idiom] = 成语字音(_list=raw_idioms[raw_idiom])
+    pass
+
     def 开始修改(text):
         # global now
         nonlocal display
         if type(text) is str:
             now = text
+            inputs.value = text
         else:
             now = inputs.value
 
@@ -164,13 +213,36 @@ def main(page: ft.Page):
         """
         放搜索的部分
         """
+        # print(display.get_state())
+        筛选(display.get_state())
         更新列表(e)
         page.update()
 
-    def 更新列表(e):
-        nonlocal lv
+    def 筛选(_dict):
+        """
+        {right: [s, y, t], maybe:[q,w],wrong:[z,x]}×
+        right{s:[(s,0)]}
+        """
+        nonlocal idioms
+        if _dict['成语'] in idioms:
+            del idioms[_dict['成语']]
+
+        s = idioms.copy()
+
+        for k, v in s.items():
+            if not all(i[0] == v.声母集合[i[1]] for i in _dict['声母']):
+                del idioms[k]
+                continue
+            if not all(i[0] == v.韵母集合[i[1]] for i in _dict['韵母']):
+                del idioms[k]
+                continue
+            if not all(i[0] == v.声调集合[i[1]] for i in _dict['声调']):
+                del idioms[k]
+                continue
+
+    def 更新列表(e=None):
         lv.controls.clear()
-        for i in ['风声鹤唳', '草木皆兵', '一板一眼', '一鼓作气', '凄凄切切']:
+        for i in list(idioms)[:10]:
             lv.controls.append(TextBox(text=str(i), click=开始修改, width=50, height=50))
         lv.update()
 
